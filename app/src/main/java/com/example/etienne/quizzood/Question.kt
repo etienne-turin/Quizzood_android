89e8918632be311d8c63ba70679ba1482e62d933
Etienne <etienne.turin@etu.univ-amu.fr>
2018-04-13 21:00:03 +0200

    Classe Question changements attributs et fonction

---
+++ app/src/main/java/com/example/etienne/quizzood/Question.kt
@@ -0,0 +1,9 @@
+class Question {
+
+    var id: Int = 0;
+    var contenu: String = "";
+
+    private val mesReponses: ArrayList<String>? = null // la bonne réponse est toujours à l'indice 0 ou pas car plusieur réponses possible ?
+
+
+} //http://tutos-android-france.com/introduction-a-kotlin/
